e34abbdae8778fa9ea8d425e1eca3dfb5bc9969e
Max Rahder <rahder@gmail.com>
2019-09-29 17:15:53 -0500

    Show album details

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Panel, DataView } from '@sencha/ext-modern';
-
 import './App.scss';
-import Foo from './Foo/Foo';
+import { Dialog, Panel } from '@sencha/ext-modern';
 import Thumbnails from './thumbnails/Thumbnails';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = { album: false };
         this.newReleases = Ext.create('Ext.data.Store', {
             fields: [
                 {
@@ -25,16 +24,33 @@ class App extends Component {
             },
             autoLoad: true
         });
-
         window.Spotify[this.constructor.name] = this; // For debugging
     }
 
     render() {
         return (
             <Panel layout="fit">
-                <Thumbnails store={this.newReleases} />
+                <Thumbnails store={this.newReleases} onChildTap={this.onChildTap.bind(this)} />
+                <Dialog
+                    displayed={!!this.state.album}
+                    title={this.state.album ? this.state.album.data.name : ''}
+                    closable
+                    closeAction="hide"
+                    maskTapHandler={dialog => dialog.hide()}
+                    onHide={() => this.setState({ album: false })}
+                    height={400}
+                    width={700}
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.'
+                </Dialog>
             </Panel>
         );
+    }
+    onChildTap(dataview, location) {
+        this.setState({ album: location.record });
+        console.log(location.record.data.name);
     }
 }
 export default App;

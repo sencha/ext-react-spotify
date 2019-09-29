255a094dfdd90ef1a36f0a93d5fe3405be3c880f
Max Rahder <rahder@gmail.com>
2019-09-29 17:18:43 -0500

    Audio preview.

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Grid, Column, Dialog, Container } from '@sencha/ext-modern';
+import { Grid, Column, Dialog, Container, Audio } from '@sencha/ext-modern';
 import './Album.scss';
 
 class Album extends Component {
     constructor(props) {
         super(props);
+        this.audio = React.createRef();
         this.state = { album: props.album };
         this.tracks = Ext.create('Ext.data.Store', {
             proxy: {
@@ -27,6 +28,21 @@ class Album extends Component {
             }
         }
     }
+    onSelect(grid, records) {
+        const record = records[0];
+        console.log(record.data.name);
+        const audio = this.audio.current.cmp;
+        if (record.data.preview_url) {
+            audio.setUrl(record.data.preview_url);
+            audio.enable();
+            audio.play();
+        } else {
+            audio.stop();
+            audio.setUrl(null);
+            audio.disable();
+        }
+    }
+
     render() {
         return (
             <Dialog
@@ -39,13 +55,20 @@ class Album extends Component {
                     this.setState({ album: false });
                     // If there is an onUnselect callback, run it.
                     this.props.onUnselect && this.props.onUnselect();
+
+                    // Reinitialize the audio player.
+                    const audio = this.audio.current.cmp;
+                    audio.stop();
+                    audio.setUrl(null);
+                    audio.disable();
                 }}
                 height={400}
                 width={700}
                 cls="foo"
                 layout={{ type: 'hbox' }}
             >
-                <Grid flex={1} store={this.tracks} hideHeaders={true}>
+                <Grid onSelect={this.onSelect.bind(this)} flex={1} store={this.tracks} hideHeaders={true}>
+                    {' '}
                     <Column text="Name" dataIndex="name" flex="1" />
                     <Column
                         dataIndex="duration_ms"
@@ -59,7 +82,7 @@ class Album extends Component {
                 </Grid>
 
                 <Container layout={{ type: 'vbox' }}>
-                    <Container cls="righttop">This will be the audio player</Container>
+                    <Audio ref={this.audio} disabled={true} />
                     <Container flex={1} cls="rightbottom">
                         This will be the chart
                     </Container>

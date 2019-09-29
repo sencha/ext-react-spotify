7d1e76883b3d532b1c8ddc069f6a40804e9f4eca
Max Rahder <rahder@gmail.com>
2019-09-29 17:17:56 -0500

    Tracks.

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { Dialog, Container } from '@sencha/ext-modern';
+import { Grid, Column, Dialog, Container } from '@sencha/ext-modern';
 import './Album.scss';
 
 class Album extends Component {
     constructor(props) {
         super(props);
         this.state = { album: props.album };
+        this.tracks = Ext.create('Ext.data.Store', {
+            proxy: {
+                type: 'ajax',
+                reader: {
+                    type: 'json',
+                    rootProperty: 'tracks.items'
+                }
+            }
+        });
         window.Spotify[this.constructor.name] = this; // For debugging
     }
     componentDidUpdate(prevProps) {
         if (this.props.album !== prevProps.album) {
             if (this.props.album) {
                 this.setState({ album: this.props.album });
+                this.tracks.load({ url: this.props.album.data.href });
             } else {
                 this.setState({ album: false });
             }
@@ -35,9 +45,19 @@ class Album extends Component {
                 cls="foo"
                 layout={{ type: 'hbox' }}
             >
-                <Container flex={1} cls="left">
-                    This will be the grid
-                </Container>
+                <Grid flex={1} store={this.tracks} hideHeaders={true}>
+                    <Column text="Name" dataIndex="name" flex="1" />
+                    <Column
+                        dataIndex="duration_ms"
+                        renderer={ms => {
+                            const minutes = Math.floor(ms / 1000 / 60);
+                            const seconds = Math.floor(ms / 1000) % 60;
+                            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+                        }}
+                        width={80}
+                    />
+                </Grid>
+
                 <Container layout={{ type: 'vbox' }}>
                     <Container cls="righttop">This will be the audio player</Container>
                     <Container flex={1} cls="rightbottom">
